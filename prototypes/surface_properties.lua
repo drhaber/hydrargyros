b2135b41827e9aca073c37976c178ac2b83b2d55
drhaber <Drhaber@live.com>
2025-07-01 16:06:18 -0400

    more forbidens

--- prototypes/surface_properties.lua
+++ prototypes/surface_properties.lua
@@ -32,7 +32,24 @@ set_causticity("corrundum", 90)
 set_causticity("moshine", 45)
 set_causticity("cubium", 30)
 
+local notHydrargyro  = {property = "hydrargyros-causticity", max = 200}
 
 for _, entity in pairs(data.raw["electric-pole"]) do
-	PlanetsLib.restrict_surface_conditions(entity, {property = "hydrargyros-causticity", max = 200})
+	PlanetsLib.restrict_surface_conditions(entity, notHydrargyro)
 end
+
+	PlanetsLib.restrict_surface_conditions(data.raw["pipe"]["pipe"], notHydrargyro)
+	PlanetsLib.restrict_surface_conditions(data.raw["pipe-to-ground"]["pipe-to-ground"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["storage-tank"]["storage-tank"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["reactor"]["nuclear-reactor"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["generator"]["steam-engine"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["generator"]["steam-turbine"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["inserter"]["inserter"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["inserter"]["long-handed-inserter"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["inserter"]["fast-inserter"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["inserter"]["bulk-inserter"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["inserter"]["stack-inserter"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["assembling-machine"]["assembling-machine-1"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["assembling-machine"]["assembling-machine-2"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["assembling-machine"]["assembling-machine-3"], notHydrargyro)
+  PlanetsLib.restrict_surface_conditions(data.raw["heat-pipe"]["heat-pipe"], notHydrargyro)
